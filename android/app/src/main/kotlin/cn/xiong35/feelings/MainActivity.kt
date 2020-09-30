a5124d4be7ba01b235e83a7f1eff8d3b3fbccc3e
xiong35 <2242787668@qq.com>
2020-09-30 21:33:50 +0800

    init

---
+++ android/app/src/main/kotlin/cn/xiong35/feelings/MainActivity.kt
@@ -0,0 +1,6 @@
+package cn.xiong35.feelings
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ docs/Daily Report.md
@@ -0,0 +1,8 @@
+
+# Daily Report
+
+## 9-30
+
+- 创建项目
+- 规划基本功能(见 [TODOs.md](./TODOs.md))
+- 
