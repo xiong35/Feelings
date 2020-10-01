56c1b9da3ac05e7de1b6e405d4cd3e1c6f48b863
xiong35 <2242787668@qq.com>
2020-10-01 10:50:48 +0800

    config color theme

---
+++ lib/global/colors.dart
@@ -0,0 +1,130 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+class FeelingsThemeData {
+  static const _lightFillColor = Colors.black87;
+  static const _darkFillColor = Colors.white70;
+
+  static final Color _lightFocusColor =
+      Colors.black.withOpacity(0.12);
+  static final Color _darkFocusColor =
+      Colors.white.withOpacity(0.12);
+
+  static ThemeData lightThemeData =
+      themeData(lightColorScheme, _lightFocusColor);
+  static ThemeData darkThemeData =
+      themeData(darkColorScheme, _darkFocusColor);
+
+  static ThemeData themeData(
+      ColorScheme colorScheme, Color focusColor) {
+    return ThemeData(
+      colorScheme: colorScheme,
+      textTheme: _textTheme,
+      // Matches manifest.json colors and background color.
+      primaryColor: const Color(0xFF030303),
+      appBarTheme: AppBarTheme(
+        textTheme: _textTheme.apply(
+            bodyColor: colorScheme.onPrimary),
+        color: colorScheme.background,
+        elevation: 0,
+        iconTheme: IconThemeData(
+            color: colorScheme.primary),
+        brightness: colorScheme.brightness,
+      ),
+      iconTheme: IconThemeData(
+          color: colorScheme.onPrimary),
+      canvasColor: colorScheme.background,
+      scaffoldBackgroundColor:
+          colorScheme.background,
+      highlightColor: Colors.transparent,
+      accentColor: colorScheme.primary,
+      focusColor: focusColor,
+      snackBarTheme: SnackBarThemeData(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Color.alphaBlend(
+          _lightFillColor.withOpacity(0.80),
+          _darkFillColor,
+        ),
+        contentTextStyle: _textTheme.subtitle1
+            .apply(color: _darkFillColor),
+      ),
+    );
+  }
+
+  static const ColorScheme lightColorScheme =
+      ColorScheme(
+    primary: Color(0xFF1E88E5),
+    primaryVariant: Color(0xFF448AFF),
+    secondary: Color(0xFF00B8D4),
+    secondaryVariant: Color(0xFF00ACC1),
+    background: Color(0xFFE6EBEB),
+    surface: Color(0xFFFAFBFB),
+    onBackground: Colors.white,
+    error: _lightFillColor,
+    onError: _lightFillColor,
+    onPrimary: _lightFillColor,
+    onSecondary: Color(0xFF80DEEA),
+    onSurface: Color(0xFF241E30),
+    brightness: Brightness.light,
+  );
+
+  static const ColorScheme darkColorScheme =
+      ColorScheme(
+    primary: Color(0xFF1E88E5),
+    primaryVariant: Color(0xFF448AFF),
+    secondary: Color(0xFF1E88E5),
+    secondaryVariant: Color(0xFF448AFF),
+    background: Color(0xFF241E20),
+    surface: Color(0xFF1F1922),
+    onBackground: Color(
+        0x0DFFFFFF), // White with 0.05 opacity
+    error: _darkFillColor,
+    onError: _darkFillColor,
+    onPrimary: _darkFillColor,
+    onSecondary: _darkFillColor,
+    onSurface: _darkFillColor,
+    brightness: Brightness.dark,
+  );
+
+  static const _regular = FontWeight.w400;
+  static const _medium = FontWeight.w500;
+  static const _semiBold = FontWeight.w600;
+  static const _bold = FontWeight.w700;
+
+  static final TextTheme _textTheme = TextTheme(
+    headline4: TextStyle(
+        fontWeight: _bold, fontSize: 20.0),
+    caption: TextStyle(
+        fontWeight: _semiBold, fontSize: 16.0),
+    headline5: TextStyle(
+        fontWeight: _medium, fontSize: 16.0),
+    subtitle1: TextStyle(
+        fontWeight: _medium, fontSize: 16.0),
+    overline: TextStyle(
+        fontWeight: _medium, fontSize: 12.0),
+    bodyText1: TextStyle(
+        fontWeight: _regular, fontSize: 14.0),
+    subtitle2: TextStyle(
+        fontWeight: _medium, fontSize: 14.0),
+    bodyText2: TextStyle(
+        fontWeight: _regular, fontSize: 16.0),
+    headline6: TextStyle(
+        fontWeight: _bold, fontSize: 16.0),
+    button: TextStyle(
+        fontWeight: _semiBold, fontSize: 14.0),
+  );
+
+  static ThemeData getTheme(String type) {
+    switch (type) {
+      case "dark":
+        return darkThemeData;
+      case "light":
+        return lightThemeData;
+    }
+    return lightThemeData;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:feelings/global/colors.dart';
+import 'package:event_bus/event_bus.dart';
+
+EventBus eventBus = EventBus();
 
 void main() {
-  runApp(MyApp());
+  runApp(Feelings());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Feelings extends StatefulWidget {
+  Feelings({Key key}) : super(key: key);
+
+  @override
+  _FeelingsState createState() => _FeelingsState();
+}
+
+class _FeelingsState extends State<Feelings> {
+  String themeType = "light";
+
+  @override
+  void initState() {
+    //注册eventBus事件，监听到变化的时候调用changeColor(color)
+    eventBus
+        .on<ThemeEvent>()
+        .listen((ThemeEvent data) {
+      changeTheme(data.theme);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Feelings',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity:
-            VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: FeelingsThemeData.getTheme(themeType),
       home: MyHomePage(
           title: 'Flutter Demo Home Page'),
     );
+  }
+
+  changeTheme(ThemeData theme) {
+    if (mounted) {
+      setState(() {
+        themeType =
+            themeType == "dark" ? "light" : "dark";
+      });
+    }
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title})
       : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,51 +61,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment:
               MainAxisAlignment.center,
           children: <Widget>[
@@ -106,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '_counter',
               style: Theme.of(context)
                   .textTheme
                   .headline4,
@@ -115,10 +85,16 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => eventBus.fire(ThemeEvent(
+            FeelingsThemeData.darkThemeData)),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class ThemeEvent {
+  ThemeData theme;
+  ThemeEvent(this.theme);
 }
