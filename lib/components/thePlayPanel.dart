ccd0ffd90ea3d9ae35e0b74c138a5d2a3943ebf4
xiong35 <2242787668@qq.com>
2020-10-07 18:29:23 +0800

    add drag to cut song

--- lib/components/thePlayPanel.dart
+++ lib/components/thePlayPanel.dart
@@ -1,5 +1,6 @@
 import 'package:feelings/components/ImgPlaceHolder.dart';
 import 'package:feelings/global/global.dart';
+import 'package:feelings/global/theMusicController.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,34 +13,44 @@ class ThePlayPanel extends StatelessWidget {
         Provider.of<MusicPlayModel>(context, listen: true);
 
     return BottomAppBar(
-      child: ListTile(
-        onTap: () => Navigator.pushNamed(context, "musicPlay",
-            arguments: {'id': 66}), //TODO: id
-        leading: Hero(
-          child: ClipOval(
-            child: ImgPlaceHolder(
-              url: musicPlayModelListen?.curSong?.al?.picUrl,
-              height: 50.0,
-              width: 50.0,
+      child: GestureDetector(
+        onHorizontalDragEnd: (d) {
+          Provider.of<MusicPlayModel>(context, listen: false)
+              .cutSong(d.primaryVelocity > 0
+                  ? SongChangeType.backward
+                  : SongChangeType.forward);
+        },
+        child: ListTile(
+          onTap: () => Navigator.pushNamed(context, "musicPlay",
+              arguments: {'id': 66}),
+          leading: Hero(
+            child: ClipOval(
+              child: ImgPlaceHolder(
+                url: musicPlayModelListen?.curSong?.al?.picUrl,
+                height: 50.0,
+                width: 50.0,
+              ),
             ),
+            tag:
+                "musicCover${musicPlayModelListen.curSong.al.id}",
           ),
-          tag:
-              "musicCover${musicPlayModelListen.curSong.al.id}",
-        ),
-        title: Text(musicPlayModelListen.curSong.name),
-        subtitle: Text(musicPlayModelListen.curSong.ar[0].name),
-        trailing: IconButton(
-          icon: Icon(
-            Provider.of<MusicPlayModel>(context, listen: true)
-                    .isPlaying
-                ? Icons.pause_circle_outline
-                : Icons.play_circle_outline,
-            color: Theme.of(context).colorScheme.primary,
+          title: Text(musicPlayModelListen.curSong.name),
+          subtitle:
+              Text(musicPlayModelListen.curSong.ar[0].name),
+          trailing: IconButton(
+            icon: Icon(
+              Provider.of<MusicPlayModel>(context, listen: true)
+                      .isPlaying
+                  ? Icons.pause_circle_outline
+                  : Icons.play_circle_outline,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: () => Provider.of<MusicPlayModel>(
+                    context,
+                    listen: false)
+                .togglePlay(),
+            iconSize: 32.0,
           ),
-          onPressed: () => Provider.of<MusicPlayModel>(context,
-                  listen: false)
-              .togglePlay(),
-          iconSize: 32.0,
         ),
       ),
     );
